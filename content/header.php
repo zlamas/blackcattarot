8cc413e76bac74909c0dabbc33876e97a5e7e4d5
Vadim I. <vadign00@gmail.com>
2024-05-13 10:35:44 +0200

    Minor fixes

--- content/header.php
+++ content/header.php
@@ -14,7 +14,7 @@
 <meta name="theme-color" content="#000000">
 <link rel="preload" href="/fonts/edo.woff2" as="font" crossorigin>
 <link rel="stylesheet" href="/css/common.css">
-<link rel="stylesheet" href="/css/main.css?15">
+<link rel="stylesheet" href="/css/main.css?16">
 
 <header class="header">
 	<input class="nav-menu-toggle" id="menu-toggle" type="checkbox">
